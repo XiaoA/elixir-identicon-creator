8e7a60c75f222221b5da922f54a6bf52f66c0c33
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-28 20:08:05 -0700

    Implement new struct for grid data

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,11 +7,14 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map(&mirror_row/1)
-    |> List.flatten
-    |> Enum.with_index
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
@@ -27,7 +30,7 @@ defmodule Identicon do
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
-      |> :binary.bin_to_list()
+      |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
